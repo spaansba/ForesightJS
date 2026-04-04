743a29cdf3dadc155900dfffa441e1b917ccbf62
Bart Spaans <bartspaans96@gmail.com>
2026-04-04 22:41:28 +0200

    prettier run

--- packages/devpage-vue/vite.config.ts
+++ packages/devpage-vue/vite.config.ts
@@ -3,12 +3,15 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueDevTools from "vite-plugin-vue-devtools"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
+      "js.foresight": path.resolve(__dirname, "../js.foresight/src/index.ts"),
+      "js.foresight-devtools": path.resolve(__dirname, "../js.foresight-devtools/src/index.ts"),
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
