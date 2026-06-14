46d1dbc50a4b6a50509d33b233f34a780d4643cb
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2026-06-14 23:01:47 +0200

    Docs: Use new `foresight` component (#142)

--- packages/docs/src/components/ForesightOverview/Overview/Keyboard/SmallButton.tsx
+++ packages/docs/src/components/ForesightOverview/Overview/Keyboard/SmallButton.tsx
@@ -1,53 +1,41 @@
-import { useForesight } from "@foresightjs/react"
+import { Foresight } from "@foresightjs/react"
 import React from "react"
 import styles from "./styles.module.css"
 
 const SmallButton = ({ index }: { index: number }) => {
-  const { elementRef, isCallbackRunning, isPredicted, durationMs } =
-    useForesight<HTMLButtonElement>({
-      callback: async () => {
+  return (
+    <Foresight
+      as="button"
+      callback={async () => {
         const randomTime = Math.floor(Math.random() * 250) + 50
         await new Promise(resolve => setTimeout(resolve, randomTime))
-      },
-      hitSlop: 0,
-      reactivateAfter: 8000,
-      meta: { buttonNr: index },
-    })
-
-  const isLoading = isCallbackRunning
-  const isLoaded = isPredicted && !isCallbackRunning
-
-  const state = () => {
-    if (isLoading) {
-      return "Fetching..."
-    }
-
-    if (isLoaded) {
-      return "Fetched"
-    }
-
-    return "Element"
-  }
-
-  return (
-    <button
-      ref={elementRef}
-      className={`${styles.smallButton} ${
-        isLoading ? styles.loading : isLoaded ? styles.loaded : styles.default
-      }`}
+      }}
+      hitSlop={0}
+      reactivateAfter={8000}
+      meta={{ buttonNr: index }}
+      className={styles.smallButton}
       id={`button-${index}`}
     >
-      <div className={styles.buttonContent}>
-        <div className={styles.buttonText}>{state()}</div>
+      {({ isCallbackRunning, isPredicted, durationMs }) => {
+        const isLoading = isCallbackRunning
+        const isLoaded = isPredicted && !isCallbackRunning
 
-        {isLoaded && (
-          <>
-            <div className={styles.timeSmall}>{Math.round(durationMs ?? 0)}ms</div>
-            <div className={styles.checkmark}>✓</div>
-          </>
-        )}
-      </div>
-    </button>
+        const state = isLoading ? "Fetching..." : isLoaded ? "Fetched" : "Element"
+
+        return (
+          <div className={styles.buttonContent}>
+            <div className={styles.buttonText}>{state}</div>
+
+            {isLoaded && (
+              <>
+                <div className={styles.timeSmall}>{Math.round(durationMs ?? 0)}ms</div>
+                <div className={styles.checkmark}>✓</div>
+              </>
+            )}
+          </div>
+        )
+      }}
+    </Foresight>
   )
 }
 
