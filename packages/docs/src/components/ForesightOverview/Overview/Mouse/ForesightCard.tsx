8bb32088b493df1ed6f736c203483a6205cc0b6d
Bart Spaans <bartspaans96@gmail.com>
2026-06-14 22:49:58 +0200

    use foresight component in docs

--- packages/docs/src/components/ForesightOverview/Overview/Mouse/ForesightCard.tsx
+++ packages/docs/src/components/ForesightOverview/Overview/Mouse/ForesightCard.tsx
@@ -1,28 +1,30 @@
 import React from "react"
 import styles from "../../styles.module.css"
 import BaseCard from "./BaseCard"
-import { useForesight } from "@foresightjs/react"
+import { Foresight } from "@foresightjs/react"
 
 export const ForesightCard = () => {
-  const { elementRef, isCallbackRunning, isPredicted } = useForesight<HTMLButtonElement>({
-    callback: async () => {
-      await new Promise(resolve => setTimeout(resolve, 300))
-    },
-    hitSlop: { left: 40, top: 50, right: 30, bottom: 50 },
-    name: "foresight-card",
-  })
-
   return (
-    <button ref={elementRef} className={styles.cardContent}>
-      <BaseCard
-        isLoaded={isPredicted && !isCallbackRunning}
-        isLoading={isCallbackRunning}
-        text={
-          <span>
-            <b>ForesightJS</b> to prefetch
-          </span>
-        }
-      />
-    </button>
+    <Foresight
+      as="button"
+      callback={async () => {
+        await new Promise(resolve => setTimeout(resolve, 300))
+      }}
+      hitSlop={{ left: 40, top: 50, right: 30, bottom: 50 }}
+      foresightName="foresight-card"
+      className={styles.cardContent}
+    >
+      {({ isPredicted, isCallbackRunning }) => (
+        <BaseCard
+          isLoaded={isPredicted && !isCallbackRunning}
+          isLoading={isCallbackRunning}
+          text={
+            <span>
+              <b>ForesightJS</b> to prefetch
+            </span>
+          }
+        />
+      )}
+    </Foresight>
   )
 }
